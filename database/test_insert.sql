29cf29bbca4afad01fb5bde4dfbb3561e4091608
bibimoni <proquadi66@gmail.com>
2025-04-25 00:54:05 +0700

    commit

--- database/test_insert.sql
+++ database/test_insert.sql
@@ -1,7 +1,7 @@
 USE shopee;
 
 CALL Proc_Insert_buyer(
-     'bibimoni',
+     'distiled',
      '1f3f26a2438cf522c5db91708afad112ec94dabdafee02a36b2e37965b2d26ae',
      'proquadi66@gmail.com',
      null,
@@ -9,7 +9,7 @@ CALL Proc_Insert_buyer(
      false,
      null,
      null,
-     null
+     0
      );
 
 SELECT * FROM Person;
