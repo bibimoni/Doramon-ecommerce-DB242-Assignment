ddc2307185f47586a3e9fa5428c010cf5b0e6d10
khanhdq109 <khanh.daoquoc@hcmut.edu.vn>
2025-05-05 02:01:30 +0700

    add another function

--- database/functions.sql
+++ database/functions.sql
@@ -4,6 +4,9 @@ DROP FUNCTION IF EXISTS Func_Valid_name;
 DROP FUNCTION IF EXISTS Exists_Admin;
 DROP FUNCTION IF EXISTS Calculate_Total_Discount;
 
+##########################################################
+################### Support functions ####################
+##########################################################
 DELIMITER //
 CREATE FUNCTION Func_Valid_name(s VARCHAR(255))
     RETURNS BOOLEAN
@@ -25,7 +28,6 @@ BEGIN
     RETURN EXISTS (SELECT 1 FROM Admin WHERE username = in_admin_usr);
 END //
 DELIMITER ;
-
 
 ##########################################################
 #### Function to calculate total discount of an order ####
@@ -75,5 +77,54 @@ BEGIN
     CLOSE voucher_cursor;
 
     RETURN totalDiscount;
+END //
+DELIMITER ;
+
+##########################################################
+### Function to calculate the average star of a product ##
+##########################################################
+DELIMITER //
+CREATE FUNCTION Calculate_Avg_Star(p_id INT)
+    RETURN DECIMAL(3, 2)
+    DETERMINISTIC
+BEGIN
+    IF p_id IS NULL OR p_id <= 0 THEN
+        RETURN 0.00
+    END IF;
+
+    DECLARE done INT DEFAULT 0;
+    DECLARE star_value TYNYINT;
+    DECLARE total INT DEFAULT 0;
+    DECLARE count_rows INT DEFAULT 0;
+    DECLARE avg_star DECIMAL(3, 2);
+
+    DECLARE star_cursor CURSOR FOR
+        SELECT star
+        FROM Comment
+        WHERE product_id = p_id;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN star_cursor;
+
+    read_loop: LOOP
+        FETCH star_cursor INTO star_value;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        SET total = total + star_value;
+        SET count_rows = count_rows + 1;
+    END LOOP;
+
+    CLOSE star_cursor;
+
+    IF count_rows = 0 THEN
+        SET avg_star = 0.00;
+    ELSE
+        SET avg_star = total / count_rows;
+    END IF;
+
+    RETURN ROUND(avg_star, 2);
 END //
 DELIMITER ;
